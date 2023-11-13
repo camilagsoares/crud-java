d117c5367df829c84fd7930415b989ad5a49f2e8
camilagsoares <camila.soares19@hotmail.com>
2023-11-13 09:16:09 -0300

    Criado controller ContatoController

--- restapi/src/main/java/br/fepi/restapi/controller/ContatoController.java
+++ restapi/src/main/java/br/fepi/restapi/controller/ContatoController.java
@@ -1,5 +1,23 @@
 package br.fepi.restapi.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.fepi.restapi.repositorio.ContatoRepositorio;
+import br.fepi.restapi.model.Contato;
+
+@RestController
+@RequestMapping("/contatos")
 public class ContatoController {
     
+    private ContatoRepositorio repositorio;
+
+    @GetMapping
+    public List<Contato> listar() {
+        return repositorio.findAll();
+    }
+
 }
